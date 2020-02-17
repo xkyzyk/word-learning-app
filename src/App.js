1859bbd319ac598d0e95094791785cac81abc2b6
xkyzyk <kyzyk@users.noreply.github.com>
2020-02-17 22:33:20 +1100

    base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AddWord from "./components/AddWord";
+import LearningList from "./components/LearningList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.state = { words: [] };
+  }
+
+  onSubmit(word) {
+    let words = [...this.state.words, word];
+    this.setState({ words: words });
+  }
+
+  onDelete(id) {
+    let words = this.state.words.filter(word => word.id !== id);
+    this.setState({ words: words });
+  }
+
+  render() {
+    return (
+      <div>
+        <AddWord addWord={this.onSubmit} />
+        <LearningList words={this.state.words} handleDelete={this.onDelete} />
+      </div>
+    );
+  }
 }
-
-export default App;
