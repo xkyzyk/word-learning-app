21c761f52e703d42300c3a3032b9c3670e6f8bad
xkyzyk <kyzyk@users.noreply.github.com>
2020-02-23 14:06:40 +1100

    update

--- src/components/AddWord.js
+++ src/components/AddWord.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import _uniqueId from "lodash/uniqueId";
 
-export default function AddWord({ handleSubmit }) {
-  const [word, setWord] = useState("");
+export default function AddWord({ addWord }) {
+  const [word, setWord] = useState({
+    word: "",
+    trigger: "",
+    definition: "",
+    id: 0,
+    known: false
+  });
   const [loading, setLoading] = useState(false);
   // constructor(props) {
   //   super(props);
@@ -22,62 +28,65 @@ export default function AddWord({ handleSubmit }) {
     const target = e.target;
     const name = target.name;
     const value = target.type === "checkbox" ? target.checked : target.value;
-    this.setState({
+    setWord({
+      ...word,
       [name]: value
     });
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    this.setState({ loading: true });
+    setLoading(true);
     fetch(
-      `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${this.state.word}?key=5757083d-e287-4958-8908-6d1c086d9548`
+      `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word.word}?key=5757083d-e287-4958-8908-6d1c086d9548`
     )
       .then(response => response.json())
       .then(data => {
         if (data[0].meta.offensive === true)
-          this.setState({ word: "ur a naughty boi" });
-        this.setState({ definition: data[0].shortdef, id: _uniqueId() });
-        this.props.addWord(this.state);
-        this.setState({
+          setWord({ ...word, word: "ur a naughty boi" });
+        setWord({ ...word, definition: data[0].shortdef, id: _uniqueId() });
+        addWord(word);
+        console.log(word);
+        setWord({
+          ...word,
           word: "",
           trigger: "",
           definition: "",
-          known: false,
-          loading: false
+          known: false
         });
+        setLoading(false);
       });
   }
 
   return (
-    <form onSubmit={this.handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <fieldset>
         <legend>Add new word:</legend>
         <input
           required
           name="word"
           placeholder="word"
-          value={this.state.word}
-          onChange={this.handleChange}
+          value={word.word}
+          onChange={handleChange}
         />
         <br></br>
         <input
           name="trigger"
           placeholder="trigger"
-          value={this.state.trigger}
-          onChange={this.handleChange}
+          value={word.trigger}
+          onChange={handleChange}
         />
         <br></br>
         <input
           name="known"
           type="checkbox"
-          checked={this.state.known}
-          onChange={this.handleChange}
+          checked={word.known}
+          onChange={handleChange}
         />
         <label htmlFor="known">known</label>
         <br></br>
-        <button onSubmit={this.addWord}>
-          {this.state.loading === true ? "Loading" : "Submit"}
+        <button onSubmit={handleSubmit}>
+          {loading === true ? "Loading" : "Submit"}
         </button>
       </fieldset>
     </form>
